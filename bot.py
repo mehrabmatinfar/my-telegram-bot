c706ef35aa349fe4c28f16fb80ac4d98534899c4
mehrabmatinfar <63049580+mehrabmatinfar@users.noreply.github.com>
2025-08-05 16:46:21 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, ContextTypes, filters
 
 # توکن رباتت
-TOKEN = 8037958434:AAGxcgYLUL4xLnXfS8MZ-SuG6u5lQlHsg-Q
+TOKEN = "8037958434:AAGxcgYLUL4xLnXfS8MZ-SuG6u5lQlHsg-Q"
 
 # تابع /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
